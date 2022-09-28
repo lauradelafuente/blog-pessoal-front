7b95d4d3e2b87dae26df226175681a450fdbfd32
unknown <laura.delafuente@outlook.com.br>
2022-09-28 12:26:45 -0300

    criando e estilizando primeiro componente com CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+       <>
+       <h1 className="titulo">Home</h1>
+       <img src="https://http.cat/400" alt="Http Cat 400" className="img"/>
+       </>
+    );
+}
+
+export default Home;

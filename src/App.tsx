54e621cee9325a542f2a55f11622b58d674e596d
unknown <laura.delafuente@outlook.com.br>
2022-09-30 15:59:13 -0300

    implementando componente login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import './App.css';
-import Home from './paginas/home/Home'
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 
 function App() {
-  return (
-    <> 
+  return(
+    <BrowserRouter>
     <Navbar />
-    <Home />
+    <div style={{ minHeight: '100vh' }}>
+    <Routes> // Antigo Switch
+    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
+    </Routes>
+    </div>
     <Footer />
-    </>
-  );
+    </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,44 +1,48 @@
 import React from "react";
 import {AppBar, Toolbar, Typography} from '@material-ui/core';
 import {Box} from '@mui/material';
+import {Link} from 'react-router-dom';
+import './Navbar.css'
 
 function Navbar() {
     return (
         <>
         <AppBar position="static">
-            <Toolbar variant="dense">
-                <Box style={{ cursor: "pointer" }} >
-                    <Typography variant="h5" color="inherit">
+            <Toolbar variant="dense" className='txt'>
+                <Box className='cursor' >
+                    <Typography variant="h5" color="inherit" className='txt' style={{fontFamily: 'Quicksand'}}>
                         BlogPessoal
                     </Typography>
                 </Box>
 
                 <Box display="flex" justifyContent="start">
-                    <Box mx={1} style={{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit" style={{fontFamily: 'Quicksand'}}>
                             home
                         </Typography>
                     </Box>
-                    <Box mx={1} style={{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit" style={{fontFamily: 'Quicksand'}}>
                             postagens
                         </Typography>
                     </Box>
-                    <Box mx={1} style={{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit" style={{fontFamily: 'Quicksand'}}>
                             temas
                         </Typography>
                     </Box>
-                    <Box mx={1} style={{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
+                    <Box mx={1} className='cursor'>
+                        <Typography variant="h6" color="inherit" style={{fontFamily: 'Quicksand'}}>
                             cadastrar tema
                         </Typography>
                     </Box>
-                    <Box mx={1} style={{ cursor: "pointer" }}>
-                        <Typography variant="h6" color="inherit">
+                    <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" style={{fontFamily: 'Quicksand'}}>
                             logout
-                        </Typography>
-                    </Box>
+                            </Typography>
+                        </Box>
+                    </Link>
                 </Box>
 
             </Toolbar>
